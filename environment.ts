b33e4b809b092419833032946bcf343542e7528a
theKonfyrm <jakubbedra@wp.pl>
2026-06-27 23:18:50 +0200

    .

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export class Environment {
-  // public static LOCALHOST_BASE_URL = 'https://gigatester-server-production.up.railway.app/';
-  public static LOCALHOST_BASE_URL = 'http://localhost:8080/';
+  public static LOCALHOST_BASE_URL = 'https://gigatester-server-production.up.railway.app/';
+  // public static LOCALHOST_BASE_URL = 'http://localhost:8080/';
 }
